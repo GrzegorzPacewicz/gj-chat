2b5949c06ee929c217a3678ee3127d8992528a61
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-25 15:16:46 +0100

    Input name added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-function App() {
+import React, { useState } from "react";
 
-  return (
-    <>
-      <h1 className="text-2xl font-bold text-amber-500">hello!</h1>
-    </>
-  )
+function App() {
+    const [inputValue, setInputValue] = useState('');
+    const [error, setError] = useState('');
+    const [userName, setUserName] = useState('')
+
+    const trimName = (name) => name.trim();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const trimmedName = trimName(inputValue);
+
+        if (trimmedName.length < 3) {
+            setError('Imię musi mieć przynajmniej 3 znaki.');
+            return;
+        }
+
+        setError('');
+        setUserName((trimmedName));
+        setInputValue('')
+
+        console.log('Imię (przycięte):', trimmedName);
+    }
+
+    return (
+        <div className="p-1">
+            <form className="flex gap-3 my-2" onSubmit={handleSubmit}>
+                <input
+                    className="w-full text-xl border-[1px] border-blue-400 rounded p-2 border-solid"
+                    value={inputValue}
+                    autoFocus
+                    type="text"
+                    onChange={(event) => {
+                        setInputValue(event.target.value);
+                        setError('');
+                    }}
+                />
+                <button className="text-xl border-[1px] border-blue-400 rounded p-2 border-solid" type="submit">
+                    Wyślij
+                </button>
+            </form>
+            {error && <p className="text-red-500">{error}</p>}
+
+            <div className="text-xl">Name: <span className="font-bold text-blue-600">{userName}</span></div>
+        </div>
+    );
 }
 
-export default App
+export default App;
