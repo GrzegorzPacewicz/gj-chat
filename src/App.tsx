7f26b7619de670a64abf54313adf5611e581ff18
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-12-27 17:30:03 +0100

    Chat added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
     const [inputValue, setInputValue] = useState("");
@@ -57,34 +57,53 @@ function App() {
     };
 
     return (
-        <div className="p-1 mt-20 w-full">
-            <form className="flex gap-3 my-2" onSubmit={handleSubmit}>
-                <input
-                    className="w-full text-xl border-[1px] border-blue-600 rounded p-2 border-solid"
-                    value={inputValue}
-                    autoFocus
-                    type="text"
-                    placeholder="Wpisz swoje imię"
-                    onChange={(event) => {
-                        setInputValue(event.target.value);
-                        setError("");
-                    }}
-                />
-                <button
-                    className="text-xl border-[1px] border-blue-600 rounded p-2 border-solid hover:text-white hover:bg-blue-600 transform-all duration-300 $"
-                    type="submit"
-                >
-                    Połącz
-                </button>
-            </form>
-            <div className="h-10 py-2">
-                {error && <p className="text-red-500">{error}</p>}
+        <>
+            <div className="p-1 mt-10 w-full">
+                <form className="flex gap-3 my-2" onSubmit={handleSubmit}>
+                    <input
+                        className="w-full text-xl border-[1px] border-blue-600 rounded p-2 border-solid"
+                        value={inputValue}
+                        autoFocus
+                        type="text"
+                        placeholder="Wpisz swoje imię"
+                        onChange={(event) => {
+                            setInputValue(event.target.value);
+                            setError("");
+                        }}
+                    />
+                    <button
+                        className="text-xl border-[1px] border-blue-600 rounded p-2 border-solid hover:text-white hover:bg-blue-600 transform-all duration-300 $"
+                        type="submit"
+                    >
+                        Połącz
+                    </button>
+                </form>
+                <div className="h-10 py-2">
+                    {error && <p className="text-red-500">{error}</p>}
+                </div>
             </div>
 
-            <div>
-                <span className="text-xl font-bold text-blue-600">{userName}</span>
-            </div>
-        </div>
+            {userName && <div className="flex w-full gap-3 h-screen flex justify-end flex-col">
+
+                <div className="flex">{userName} wiadomość</div>
+
+                <div className="flex items-center gap-3 mb-60">
+                    <p className="text-xl font-bold text-blue-600">{userName}</p>
+                    <input type="text"
+                           className="w-full text-xl border-[1px] border-blue-600 rounded p-2 border-solid"
+                           onClick={() => {
+                           }} placeholder="wpisz coś"/>
+                    <button
+                        className="text-xl border-[1px] border-blue-600 rounded p-2 border-solid hover:text-white hover:bg-blue-600 transform-all duration-300 $"
+                        type="submit"
+                    >Wyślij
+                    </button>
+                </div>
+
+            </div>}
+
+        </>
+
     );
 }
 
